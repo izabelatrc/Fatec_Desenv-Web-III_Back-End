e20abe039796cdafba4b96bae184f1cd9194a7ad
izabelatrc <izabela.coimbra@fatec.sp.gov.br>
2024-04-23 22:04:55 -0300

    continuação api - query, params, install yarn, nodemom

--- aula1/index.js
+++ aula1/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const server = express();
 
+//Query params = ?nome=NodeJS
+//Route params = /curso/2
+//Request body = {nome: 'NodeJS', tipo: 'Backend'}
 //http://localhost:3000/curso
-server.get('/curso', (req, res) => {
-    return res.json({curso: 'Desenvolvimento Web III'});
-})
+/*server.get('/curso/:id', (req, res) => {
+    const nome = req.query.nome;
+    const id = req.params.id;
+
+    return res.json({curso: `${id}`, nome: `${nome}`});
+});*/
+
+const cursos = ['NodeJS', 'JavaScript', 'PHP', 'ReactJS', 'VueJS'];
+
+server.get('/curso/:index', (req, res) => {
+    const { index } = req.params;
+
+    return res.json(cursos[index]);
+});
 
 server.listen(3000);
